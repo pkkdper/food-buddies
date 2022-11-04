8f430fe8157167e5dd81380dfe60a6558693b7a5
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-11-04 07:12:15 +0100

    lu-add route structure

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,29 +9,43 @@ const app = require('../app')
 router.get('/signup', (req, res) => {
   res.render('auth/signup')
 })
-router.get('/profile', (req, res) => {
-    res.render('user/profile')
-})
 
+
+// setting POST signup route
 router.post("/signup", async (req, res) => {
-    try {
-      console.log("The form data: ", req.body);
-      const salt = bcrypt.genSaltSync(10);
-      const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-      await User.create({
-        email: req.body.email,
-        username: req.body.username,
-        password: hashedPassword,
-      });
-      res.redirect("/profile");
-    } catch (error) {
-      console.log(error);
-      res.render('user/profile', { errorMessage: error.message, isConnected: false })
-    }
-  });
+  try {
+    console.log("The form data: ", req.body);
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    await User.create({
+      email: req.body.email,
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    res.redirect("/profile");
+  } catch (error) {
+    console.log(error);
+    res.render('user/profile', { errorMessage: error.message, isConnected: false })
+  }
+});
 
-router.get('/login', (req, res) => {
+  // setting login route
+  router.get('/login', (req, res) => {
     res.render('auth/login')
   })
   module.exports = router;
 
+  // setting POST login route
+
+  
+
+
+// setting profile route
+router.get('/profile', (req, res) => {
+    res.render('user/profile')
+})
+
+
+// setting logout route
+
+module.exports = router;
