bcb87a5b9546107999a365fa95dd6da67606b0f3
pkkdpr <oliwia.giercza@gmail.com>
2022-11-07 05:57:59 -0500

    updated

--- config/config-recipes.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose
-  .connect('mongodb://localhost/food-buddies')
-  .then(x =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
-  .catch(err => console.error('Error connecting to mongo', err));
