49b7dc47f9956a47fc0c5f84ad0376fe3d3580c2
Iskandar Froede <iskandar.lucyane@gmail.com>
2022-11-08 14:46:25 +0100

    lu-step23

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -8,16 +8,29 @@ const app = require('../app')
 router.get('/home-food-buddies', async (req, res, next) => {
   console.log(req.query)
   try {
-  
     const selectedRecipes = await Recipe.find({continent: req.query.continent, meal: req.query.meal})
     console.log(selectedRecipes)
-    res.render('home-food-buddies')
+    res.render('home-food-buddies', {selectedRecipes})
+    
   } 
   catch (err) {
     console.log(err)
   }
 })
 
+      
+router.get('/add-recipe/:recipeId', async (req, res, next) => {
+  const { recipeId } = req.params
+  const userId = req.session.user._id
+
+  try {
+   await User.findByIdAndUpdate(userId, {$push:{likedRecipes: recipeId}})
+   res.redirect('/auth/profile')
+  } 
+  catch (err) {
+    console.log(err)
+  }
+})
 
 
 
