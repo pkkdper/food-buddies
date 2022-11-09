104a726dd14caa7dd98573096f893ec9ca5e06d1
pkkdpr <oliwia.giercza@gmail.com>
2022-11-09 09:08:31 -0500

    css

--- config/session.config.js
+++ config/session.config.js
@@ -13,7 +13,7 @@ module.exports = app => {
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 60000, 
+        maxAge: 60000*60*24*7, 
       },
       store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1/food-buddies',
