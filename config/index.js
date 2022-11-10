0b4d1b8302e87a0ddd61578e749ace90bb576cf9
pkkdpr <oliwia.giercza@gmail.com>
2022-11-10 05:07:05 -0500

    deployment

--- config/index.js
+++ config/index.js
@@ -17,27 +17,6 @@ const favicon = require("serve-favicon");
 // https://www.npmjs.com/package/path
 const path = require("path");
 
-
-
-
-
-// google API thing ?????
-const session = require('express-session');
-
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'SECRET' 
-}));
-
-app.get('/', function(req, res) {
-  res.render('pages/auth');
-});
-
-
-
-
-
 // Middleware configuration
 module.exports = (app) => {
   // In development environment the app logs
@@ -58,57 +37,3 @@ module.exports = (app) => {
   // Handles access to the favicon
   app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
 };
-
-
-
-
-/*  PASSPORT SETUP  */
-
-const passport = require('passport');
-var userProfile;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.set('view engine', 'ejs');
-
-app.get('/success', (req, res) => res.send(userProfile));
-app.get('/error', (req, res) => res.send("error logging in"));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-
-
-
-
-/*  Google AUTH  */
- 
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = '811372085495-diab4hkkon9tblgf97osv0lje6u00gmb.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      userProfile=profile;
-      return done(null, userProfile);
-  }
-));
- 
-app.get('/auth/google', 
-  passport.authenticate('google', { scope : ['profile', 'email'] }));
- 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/error' }),
-  function(req, res) {
-    // Successful authentication, redirect success.
-    res.redirect('/success');
-  });
